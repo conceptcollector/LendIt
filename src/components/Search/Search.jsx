61c2c333c7ca2c6de02812e63b339f93ae0db38b
Christopher Boyd <boyd.christopher.t@gmail.com>
2022-08-01 14:21:01 -0500

    On further thought, autocomplete probably isn't the way to go

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,20 +1,21 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { Autocomplete, Box, Button, Stack, TextField } from '@mui/material';
+import { Box, Button, Stack, TextField } from '@mui/material';
 
 function Search() {
-    const [ value, setValue ] = useState();
-    const [ inputValue, setInputValue ] = useState();
+    // const [ value, setValue ] = useState();
+    // const [ inputValue, setInputValue ] = useState();
     const dispatch = useDispatch();
     const items = useSelector((store) => store.items);
-    // const history = useHistory();
+    const history = useHistory();
+    const handleSearch = () => {
+        console.log('Yo');
+    }
 
     useEffect(() => {
         dispatch({ type: 'FETCH_ITEMS' });
     }, []);
-
-    console.log('*******************************************', value);
 
     return (
         <Box
@@ -26,41 +27,25 @@ function Search() {
                 display="flex"
                 flexDirection="column"
             >
-                <Autocomplete
-                    id="search-bar"
-                    freeSolo
-                    options={items.itemsReducer.map((option) => option)}
-                    // options={items.itemsReducer.map((option) => option.title)}
-                    // renderOption={(e, option) => {
-                    //     e.preventDefault();
-                    //     <Link to="/details" + {option}.id />
-                    // }}
-                    getOptionLabel={(option) => `${option.title}`}
-                    sx={{
-                        backgroundColor: 'white',
-                        width: 300
-                    }}
-                    value={value}
-                    inputValue={inputValue}
-                    onChange={(event, newValue) => {
-                        setValue(newValue);
-                    }}
-                    onInputChange={(event, newInputValue) => {
-                        setInputValue(newInputValue);
-                    }}
-                    renderInput={(params) => 
-                        <TextField
-                            {...params}
-                            variant="standard"
-                            label="Search"
-                        />}
-                />
-                <Button
-                    variant="contained"
-                    sx={{ width: 300 }}
+                <form
+                    onSubmit={handleSearch}
                 >
-                    Search
-                </Button>
+                    <TextField
+                        variant="standard"
+                        label="Search"
+                        sx={{
+                            width: 300,
+                            backgroundColor: 'white'
+                        }}
+                    />
+                    <Button
+                        variant="contained"
+                        type="submit"
+                        sx={{ width: 300 }}
+                    >
+                        Search
+                    </Button>
+                </form>
             </Box>
         </Box>
     )
